79b870a5bb7f0a6cf9e48781679172775c4fd8e5
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-08-17 20:19:55 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -950,6 +950,13 @@ async def settings(client, message):
     except KeyError:
         await save_group_settings(grp_id, 'max_btn', False)
         settings = await get_settings(grp_id)
+        
+    try:
+        if settings['auto_ffilter']:
+            await auto_filter(client, message)
+    except KeyError:
+        await save_group_settings(grp_id, 'auto_ffilter', False)
+        settings = await get_settings(grp_id)
 
     if 'is_shortlink' not in settings.keys():
         await save_group_settings(grp_id, 'is_shortlink', False)
