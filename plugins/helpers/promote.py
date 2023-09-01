71287c3feb8f2597f53a175aa526f322ce318947
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-01 22:39:33 +0530

    Update promote.py

--- plugins/helpers/promote.py
+++ plugins/helpers/promote.py
@@ -38,11 +38,9 @@ async def promote_user(client, message):
             can_change_info=True,
             can_send_messages=True,
             can_send_media_messages=True,
-            can_send_other_messages=True,
             can_invite_users=True,
             can_send_polls=True,
-            can_pin_messages=True,
-            can_add_web_page_previews=True 
+            can_pin_messages=True
         )
 
         await client.promote_chat_member(message.chat.id, user_id, permissions)
