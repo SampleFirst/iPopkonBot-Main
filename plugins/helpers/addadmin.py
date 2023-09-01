37bb2a4f51917140a2f910fc066f55ce2cabba4b
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-02 00:53:52 +0530

    Update addadmin.py

--- plugins/helpers/addadmin.py
+++ plugins/helpers/addadmin.py
@@ -13,20 +13,10 @@ async def add_admins(client, message):
     channel_id = int(message.command[2])
     
     try:
-        chat = await client.get_chat(channel_id)
-        bot_member = await chat.get_member(client.me.id)
-
         await client.promote_chat_member(
             chat_id=channel_id,
             user_id=user_id,
-            can_change_info=bot_member.can_change_info,
-            can_post_messages=bot_member.can_post_messages,
-            can_edit_messages=bot_member.can_edit_messages,
-            can_delete_messages=bot_member.can_delete_messages,
-            can_invite_users=bot_member.can_invite_users,
-            can_restrict_members=bot_member.can_restrict_members,
-            can_pin_messages=bot_member.can_pin_messages,
-            can_promote_members=bot_member.can_promote_members
+            ChatMember()
         )
 
         await message.reply(f"Admin with user ID {user_id} added to the channel with custom privileges.")
