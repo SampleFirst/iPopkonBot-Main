2f67f2ce8710cd76283c8e5f07121f299b22f7c0
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-02 02:33:15 +0530

    Update addadmin.py

--- plugins/helpers/addadmin.py
+++ plugins/helpers/addadmin.py
@@ -1,12 +1,12 @@
-from pyrogram import Client, filters
-from pyrogram.types import ChatMember, ChatPermissions
+from pyrogram import Client, filters, enums
+from pyrogram.types import ChatMember, ChatPrivileges
 from info import *
 
 # Define the add_admins command handler
 @Client.on_message(filters.command("add_admins") & filters.user(ADMINS))
 async def add_admins(client, message):
     if len(message.command) != 3:
-        await message.reply("Usage: /add_admins <user_id> <channel_id>")
+        await message.reply("Usage: /add_admins user_id channel_id")
         return
 
     user_id = int(message.command[1])
@@ -16,10 +16,10 @@ async def add_admins(client, message):
         # Create a ChatMember object with status ADMINISTRATOR
         admin_member = ChatMember(
             status=enums.ChatMemberStatus.ADMINISTRATOR,
-            user=None,  # You can provide user information here if needed
-            chat=None,  # You can provide chat information here if needed
+            user=await client.get_users(user_id),  # Provide user information
+            chat=await client.get_chat(channel_id),  # Provide chat information
             can_be_edited=False,  # Customize as needed
-            permissions=ChatPermissions(),  # Customize permissions as needed
+            privileges=ChatPrivileges(),  # Customize permissions as needed
         )
 
         await client.promote_chat_member(channel_id, user_id, admin_member)
