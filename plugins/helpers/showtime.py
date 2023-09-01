bc0646a42a848a9531acafb90f9baa058704f3c1
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-02 03:32:29 +0530

    Update showtime.py

--- plugins/helpers/showtime.py
+++ plugins/helpers/showtime.py
@@ -1,12 +1,13 @@
-from datetime import datetime
+import time  # Import the 'time' module separately
+import datetime
 from pyrogram import Client, filters
-from info import *
+from info import ADMINS 
 
 @Client.on_message(filters.command("showtime") & filters.user(ADMINS))
 def show_time(client, message):
     chat_id = message.chat.id
 
     while True:
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         client.send_message(chat_id, f"Current Date and Time: {current_time}")
-        time.sleep(1)
+        time.sleep(1)  # Use the 'time' module for sleep
