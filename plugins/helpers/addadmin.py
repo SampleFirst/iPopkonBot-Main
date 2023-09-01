f2da2408fadb77ef0cb09965e58925d84a3772cc
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-01 20:53:26 +0530

    Update addadmin.py

--- plugins/helpers/addadmin.py
+++ plugins/helpers/addadmin.py
@@ -1,19 +1,18 @@
 from pyrogram import Client, filters
 from pyrogram.types import ChatPrivileges
-import asyncio
-from info import ADMINS 
+from info import *
 
 
 # Define the add_admins command handler
-@Client.on_message(filters.command("add_admins") & filters.user("ADMINS"))
-async def add_admins_command(client, message):
+@Client.on_message(filters.command("add_admins") & filters.user(ADMINS))
+async def add_admins(client, message):
     try:
         # Get the chat ID and user ID from the message text
         chat_id = message.chat.id
-        user_id = message.text.split(" ")[1]
+        user_id = int(message.text.split(" ")[1])
 
-        # Promote the user to admin with desired privileges
-        privileges = ChatPrivileges(
+        # Define desired chat permissions for the new admin
+        privileges = chat_privileges(
             can_change_info=True,
             can_post_messages=True,
             can_edit_messages=True,
@@ -26,13 +25,9 @@ async def add_admins_command(client, message):
             can_manage_chat=True
         )
 
-        result = await client.promote_chat_member(chat_id, user_id, privileges)
+        # Promote the user to admin with desired permissions
+        await client.promote_chat_member(chat_id, user_id, ChatPrivileges=privileges)
 
-        if result:
-            await message.reply("User has been added as an admin.")
-        else:
-            await message.reply("Failed to add user as an admin.")
-
+        await message.reply("User has been added as an admin.")
     except Exception as e:
         await message.reply(f"An error occurred: {str(e)}")
- 
