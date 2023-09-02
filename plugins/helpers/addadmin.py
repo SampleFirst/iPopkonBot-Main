9376d33160a41c5a5937bc1d27ea1aeb5ef03f5a
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-02 22:23:49 +0530

    Update addadmin.py

--- plugins/helpers/addadmin.py
+++ plugins/helpers/addadmin.py
@@ -13,19 +13,22 @@ async def add_admins(client, message):
     channel_id = int(message.command[2])
     
     try:
-        # Create a ChatMember object with status ADMINISTRATOR
+        # Get the user and chat objects
+        user = await client.get_users(user_id)
+        chat = await client.get_chat(channel_id)
+
+        # Create a ChatMember object with custom privileges
         admin_member = ChatMember(
             status=enums.ChatMemberStatus.ADMINISTRATOR,
-            user=await client.get_users(user_id),  # Provide user information
-            chat=await client.get_chat(channel_id),  # Provide chat information
-            can_be_edited=True,  # Set to True, indicating this member can be edited
-            privileges=ChatPrivileges(can_promote_members=True, is_anonymous=False),  # Customize permissions as needed
+            user=user,
+            chat=chat,
+            can_be_edited=True,
+            can_promote_members=True  # Set the privilege directly
         )
 
+        # Promote the user to an administrator
         await client.promote_chat_member(channel_id, user_id, admin_member)
 
         await message.reply(f"Admin with user ID {user_id} added to the channel with custom privileges and can be edited.")
     except Exception as e:
         await message.reply(f"An error occurred: {str(e)}")
-        
-        
