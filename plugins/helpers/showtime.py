1edc696f29e4bf889a874ead008aecbcf787a03a
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-02 22:12:02 +0530

    Update showtime.py

--- plugins/helpers/showtime.py
+++ plugins/helpers/showtime.py
@@ -3,19 +3,22 @@ import datetime
 from pyrogram import Client, filters
 from info import ADMINS
 
-@Client.on_message(filters.command("showtime") & filters.user(ADMINS))
-def show_time(client, message):
-    chat_id = message.chat.id
-    current_message = None
+# Define a global variable to track whether the loop should continue
+should_continue = True
 
-    while True:
+@Client.on_message(filters.command("showtime") & filters.user(ADMINS))
+async def show_time(client, message):
+    global should_continue  # Declare should_continue as a global variable
+    chat_id = message.chat.id
+    sent_message = await client.send_message(chat_id, "Initializing...")  # Initial message
+
+    while should_continue:
         current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        if current_message is not None:
-            try:
-                client.edit_message_text(chat_id, current_message.message_id, f"Current Date and Time:\n{current_time}")
-            except Exception as e:
-                print(f"Error editing message: {e}")
-        else:
-            current_message = client.send_message(chat_id, f"Current Date and Time:\n{current_time}")
-        
+        await client.edit_message_text(chat_id, sent_message.message_id, f"Current Date and Time: {current_time}")
         time.sleep(1)
+
+@Client.on_message(filters.command("stopshowtime") & filters.user(ADMINS))
+async def stop_show_time(client, message):
+    global should_continue  # Declare should_continue as a global variable
+    should_continue = False
+    await client.send_message(message.chat.id, "Stopped updating time.")
