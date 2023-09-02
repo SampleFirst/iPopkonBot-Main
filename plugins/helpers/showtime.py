c887581effb0ed99b2c8abd771aa1100d429a01a
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-02 21:55:55 +0530

    Update showtime.py

--- plugins/helpers/showtime.py
+++ plugins/helpers/showtime.py
@@ -1,13 +1,21 @@
-import time  # Import the 'time' module separately
+import time
 import datetime
 from pyrogram import Client, filters
-from info import ADMINS 
+from info import ADMINS
 
 @Client.on_message(filters.command("showtime") & filters.user(ADMINS))
 def show_time(client, message):
     chat_id = message.chat.id
+    current_message = None
 
     while True:
         current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        client.send_message(chat_id, f"Current Date and Time: {current_time}")
-        time.sleep(1)  # Use the 'time' module for sleep
+        if current_message is not None:
+            try:
+                client.edit_message_text(chat_id, current_message.message_id, f"Current Date and Time:\n{current_time}")
+            except Exception as e:
+                print(f"Error editing message: {e}")
+        else:
+            current_message = client.send_message(chat_id, f"Current Date and Time:\n{current_time}")
+        
+        time.sleep(1)
