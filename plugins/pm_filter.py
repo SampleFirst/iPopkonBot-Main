2ab2266502e9fff7c81a48a5d1fafa0d37712f21
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-25 20:17:35 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -316,11 +316,6 @@ async def pm_text(bot, message):
             quote=True
         )
 
-        # Schedule a task to delete the reply_message after 30 seconds (adjust as needed)
-        await asyncio.sleep(30)
-        await message.delete()
-        await reply_message.delete()
-        
         # Send the log message to the specified channel with a button to show user info
         buttons = [
             [
